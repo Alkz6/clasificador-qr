b92c11f2eed3e3c931581f1b3cd38875f9216f43
Alexis <jalexismtz.medina@gmail.com>
2019-11-22 22:59:29 -0600

    Se modifico la ruta de BASE_DIR, debido a que se movio los settings

--- clasificador/settings/production.py
+++ clasificador/settings/production.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'clasificador.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, '..', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "..", "static"),
 )
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
