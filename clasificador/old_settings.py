622dde07f41871639695341819fc87352f5e7915
Alexis <jalexismtz.medina@gmail.com>
2019-11-22 22:44:55 -0600

    algunos cambios para poublicar produccion

--- clasificador/old_settings.py
+++ clasificador/old_settings.py
@@ -121,12 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
